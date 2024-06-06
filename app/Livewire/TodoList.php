89e483ae9cd93cece8380cbb88d26400bf514d35
AjiMuhammad Sofwan <ajimsofwan@gmail.com>
2024-06-06 14:17:56 +0700

    feat:create, search, paginate

---
+++ app/Livewire/TodoList.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Todo;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class TodoList extends Component
+{
+  use WithPagination;
+
+  #[Rule('required|min:3|max:50')]
+  public $name;
+
+  public $search = 1;
+
+  public function messages()
+  {
+    return [
+      'required' => 'Harus diisi.',
+      'min' => 'Minimal :min karakter',
+      'max' => 'Maksimal :max karakter',
+    ];
+  }
+
+  public function create()
+  {
+    $validated = $this->validateOnly('name');
+    Todo::create($validated);
+    $this->reset('name');
+    session()->flash('success', 'Tersimpan.');
+  }
+
+  public function render()
+  {
+    return view('livewire.todo-list', [
+      'todos' => Todo::latest()->where('name', 'like', "%{$this->search}%")->paginate(5)
+    ]);
+  }
+}
